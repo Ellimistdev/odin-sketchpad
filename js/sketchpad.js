6995a247202e11f2eb9c67ffdfbc10bd2793b29c
Nick Rodriguez <endotnick@gmail.com>
2017-06-12 23:11:09 +0000

    add hover, extracts generate function

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -1,16 +1,25 @@
 //prevents ESLint warnings in cloud 9 IDE
 /* global $ */
-$(document).ready(function(){
+$(document).ready(main());
+function main(){
+    generate(16);
+    
+    $('.square').on('mouseenter', function(){
+        $(this).addClass('highlight');
+    });
+    $('.dimension').on('keydown', function(){
+       $('.dimension').next.text(" x " + document.getElementById('dimension').value); 
+    });
+};
+function generate(count){
     var $container = $('.container');
-    var count = 16;
+    $container.empty();
     for (var i = 0; i < count; i++){
         $container.append('<tr class="row">row ' + i + ' </tr>');
         var child = $container.find('tr:last');
         for (var j = 0; j < count; j++)
         {
-            child.append('<div class="square"></div>');
+            child.append('<td class="square"></td>');
         }
     }
-    alert($('.row').length);
-    alert($('.square').length);
-});
+}
